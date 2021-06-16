fe135f8bb1040bfcafa045c0b593fada22d01144
josvillegas <josvillegas@upeu.edu.pe>
2021-06-15 21:40:20 -0500

    test

---
+++ pages/email.php
@@ -0,0 +1,18 @@
+<?php
+//get data from form
+$email = $_POST['email'];
+
+$to = "josvillegas@upeu.edu.pe";
+
+$subject = "correo desde web";
+$txt = "Email = " . $email ;
+
+$headers = "From: josvillegas@upeu.edu.pe" . "\r\n" .
+"CC: somebyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+    header("index.html");
+}
+//redirect
+//
+?>
